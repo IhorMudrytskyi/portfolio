4e6f4bb44784ef86e53cf180bbc3caab98795d3c
IhorMudrytskyi <migor2k05@gmail.com>
2026-05-30 17:23:51 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,218 @@
+# __________________________Cisco 5 семестр__________________________
+image_cisco_5sem = ["/image/5sem/cyber/cisco/Computer Hardware Basics.png",
+                    "/image/5sem/cyber/cisco/Operating Systems Basics.png",
+                    "/image/5sem/cyber/cisco/Networking Basics.png",
+                    "/image/5sem/cyber/cisco/Networking Devices and Initial Configuration.png",
+                    "/image/5sem/cyber/cisco/Network Addressing and Basic Troubleshooting.png",
+                    "/image/5sem/cyber/cisco/Network Support and Security.png",
+                    "/image/5sem/cyber/cisco/Introduction to IoT.png",
+                    "/image/5sem/cyber/cisco/Introduction to Cybersecurity.png",
+                    "/image/5sem/cyber/cisco/Ethical Hacker.png",
+                    "/image/5sem/cyber/cisco/Network Defense.png"]
+
+name_cisco_5sem = ["Computer Hardware Basics",
+        "Operating Systems Basics",
+        "Networking Basics",
+        "Networking Devices and Initial Configuration",
+        "Network Addressing and Basic Troubleshooting",
+        "Network Support and Security",
+        "Introduction to IoT",
+        "Introduction to Cybersecurity",
+        "Ethical Hacker",
+        "Network Defense"]
+
+link_cisco_5sem = ["https://www.credly.com/badges/7742fca4-2a61-47b7-98ba-b751de49956c",
+                    "https://www.credly.com/badges/e784ddc9-768a-40c1-a5d1-0bf8c18bf9d0",
+                    "https://www.credly.com/badges/0f40566a-ac58-4a6f-9041-67276733a611",
+                    "https://www.credly.com/badges/686e7b14-cc4d-4236-bf1e-24de5967aca6",
+                    "https://www.credly.com/badges/682a5433-7079-4826-b96d-02bc8f3fc7c1",
+                    "https://www.credly.com/badges/0791bbb3-7253-42ef-a796-60c0d62ab6c5",
+                    "https://www.credly.com/badges/eb79ca2c-552d-418d-8169-4caf8be4654d",
+                    "https://www.credly.com/badges/b6079468-89e2-4892-a550-f0e3d3d3a24a",
+                    "https://www.credly.com/badges/76ddbbe2-37b2-4962-bc0f-4f10c43597d4",
+                    "https://www.credly.com/badges/3478c9a4-32c1-446c-b85a-41be47e4edb8"]
+
+# __________________________HackTheBox Machines 5 семестр__________________________
+image_htb_mach_5sem = ["/image/5sem/cyber/htb/mach/Conversor.png",
+                     "/image/5sem/cyber/htb/mach/Expressway.png",
+                     "/image/5sem/cyber/htb/mach/Soulmate.png",
+                     "/image/5sem/cyber/htb/mach/Outbound.png"]
+
+name_htb_mach_5sem = ["Conversor",
+                     "Expressway",
+                     "Soulmate",
+                     "Outbound"]
+
+link_htb_mach_5sem = ["https://labs.hackthebox.com/achievement/machine/1935867/787",
+                     "https://labs.hackthebox.com/achievement/machine/1935867/736",
+                     "https://labs.hackthebox.com/achievement/machine/1935867/721",
+                     "https://labs.hackthebox.com/achievement/machine/1935867/672"]
+
+# __________________________HackTheBox Challanges 5 семестр__________________________
+image_htb_chal_5sem = ["/image/5sem/cyber/htb/chal/CubeMadness1.svg",
+                       "/image/5sem/cyber/htb/chal/You know 0xDiablos.svg",
+                       "/image/5sem/cyber/htb/chal/CubeMadness2.svg",
+                       "/image/5sem/cyber/htb/chal/Broken Shell.svg",
+                       "/image/5sem/cyber/htb/chal/LightningFast.svg"]
+
+name_htb_chal_5sem = ["CubeMadness1",
+                      "You know 0xDiablos",
+                      "CubeMadness2",
+                      "Broken Shell",
+                      "LightningFast"]
+
+link_htb_chal_5sem = ["https://labs.hackthebox.com/achievement/challenge/1935867/302",
+                      "https://labs.hackthebox.com/achievement/challenge/1935867/106",
+                      "https://labs.hackthebox.com/achievement/challenge/1935867/305",
+                      "https://labs.hackthebox.com/achievement/challenge/1935867/883",
+                      "https://labs.hackthebox.com/achievement/challenge/1935867/315"]
+
+# __________________________HackTheBox Modules 5 семестр__________________________
+image_htb_mod_5sem = ["/image/5sem/cyber/htb/mod/Setting Up.png",
+                      "/image/5sem/cyber/htb/mod/Linux Fundamentals.png",
+                      "/image/5sem/cyber/htb/mod/Windows Fundamentals.jpg",
+                      "/image/5sem/cyber/htb/mod/MacOS Fundamentals.png"]
+
+name_htb_mod_5sem = ["Setting Up",
+                     "Linux Fundamentals",
+                     "Windows Fundamentals",
+                     "MacOS Fundamentals"]
+
+link_htb_mod_5sem = ["https://academy.hackthebox.com/achievement/1315273/87",
+                     "https://academy.hackthebox.com/achievement/1315273/18",
+                     "https://academy.hackthebox.com/achievement/1315273/49",
+                     "https://academy.hackthebox.com/achievement/1315273/157"]
+
+# __________________________TryHackMe 5 семестр__________________________
+image_thm_5sem = ["/image/5sem/crypto/thm/Cryptography Basics.png",
+                      "/image/5sem/crypto/thm/Encryption - Crypto 101.png",
+                      "/image/5sem/crypto/thm/Introduction to CryptOps.png",
+                      "/image/5sem/crypto/thm/CyberChef The Basics.png"]
+
+name_thm_5sem = ["Cryptography Basics",
+                     "Encryption - Crypto 101",
+                     "Introduction to CryptOps",
+                     "CyberChef: The Basics"]
+
+# __________________________HackTheBox Modules 6 семестр__________________________
+image_htb_mod_6sem = ["/image/6sem/cyber/mod/Setting Up.png",
+                      "/image/6sem/cyber/mod/Linux Fundamentals.png",
+                      "/image/6sem/cyber/mod/Windows Fundamentals.jpg",
+                      "/image/6sem/cyber/mod/MacOS Fundamentals.png",
+                      "/image/6sem/cyber/mod/Introduction to Networking.png",
+                      "/image/6sem/cyber/mod/Intro to Network Traffic Analysis.png",
+                      "/image/6sem/cyber/mod/Web Requests.png",
+                      "/image/6sem/cyber/mod/Introduction to Active Directory.png",
+                      "/image/6sem/cyber/mod/Introduction to Web Applications.png",
+                      "/image/6sem/cyber/mod/Security Incident Reporting.png",
+                      "/image/6sem/cyber/mod/Using the Metasploit Framework.png",
+                      "/image/6sem/cyber/mod/Attacking Web Applications with Ffuf.png",
+                      "/image/6sem/cyber/mod/Vulnerability Assessment.png",
+                      "/image/6sem/cyber/mod/File Inclusion.jpg",
+                      "/image/6sem/cyber/mod/File Transfers.png",
+                      "/image/6sem/cyber/mod/SQL Injection Fundamentals.png",
+                      "/image/6sem/cyber/mod/Brief Intro to Hardware Attacks.png"]
+
+name_htb_mod_6sem = ["Setting Up",
+                     "Linux Fundamentals",
+                     "Windows Fundamentals",
+                     "MacOS Fundamentals",
+                     "Introduction to Networking",
+                     "Intro to Network Traffic Analysis",
+                     "Web Requests",
+                     "Introduction to Active Directory",
+                     "Introduction to Web Applications",
+                     "Security Incident Reporting",
+                     "Using the Metasploit Framework",
+                     "Attacking Web Applications with Ffuf",
+                     "Vulnerability Assessment",
+                     "File Inclusion",
+                     "File Transfers",
+                     "SQL Injection Fundamentals",
+                     "Brief Intro to Hardware Attacks"]
+
+link_htb_mod_6sem = ["https://academy.hackthebox.com/achievement/1315273/87",
+                     "https://academy.hackthebox.com/achievement/1315273/18",
+                     "https://academy.hackthebox.com/achievement/1315273/49",
+                     "https://academy.hackthebox.com/achievement/1315273/157",
+                     "https://academy.hackthebox.com/achievement/1315273/34",
+                     "https://academy.hackthebox.com/achievement/1315273/81",
+                     "https://academy.hackthebox.com/achievement/1315273/35",
+                     "https://academy.hackthebox.com/achievement/1315273/74",
+                     "https://academy.hackthebox.com/achievement/1315273/75",
+                     "https://academy.hackthebox.com/achievement/1315273/238",
+                     "https://academy.hackthebox.com/achievement/1315273/39",
+                     "https://academy.hackthebox.com/achievement/1315273/54",
+                     "https://academy.hackthebox.com/achievement/1315273/108",
+                     "https://academy.hackthebox.com/achievement/1315273/23",
+                     "https://academy.hackthebox.com/achievement/1315273/24",
+                     "https://academy.hackthebox.com/achievement/1315273/33",
+                     "https://academy.hackthebox.com/achievement/1315273/230"]
+
+# __________________________HackTheBox Machines 6 семестр__________________________
+image_htb_mach_6sem = ["/image/6sem/cyber/mach/Facts.png",
+                       "/image/6sem/cyber/mach/Interpreter.png",
+                       "/image/6sem/cyber/mach/DevArea.png",
+                       "/image/6sem/cyber/mach/CCTV.png",
+                       "/image/6sem/cyber/mach/Silentium.png",
+                       "/image/6sem/cyber/mach/Kobold.png",
+                       "/image/6sem/cyber/mach/WingData.png",
+                       "/image/6sem/cyber/mach/MonitorsFour.png"]
+
+name_htb_mach_6sem = ["Facts",
+                      "Interpreter",
+                      "DevArea",
+                      "CCTV",
+                      "Silentium",
+                      "Kobold",
+                      "WingData",
+                      "MonitorsFour"]
+
+link_htb_mach_6sem = ["https://labs.hackthebox.com/achievement/machine/1935867/829",
+                      "https://labs.hackthebox.com/achievement/machine/1935867/841",
+                      "https://labs.hackthebox.com/achievement/machine/1935867/859",
+                      "https://labs.hackthebox.com/achievement/machine/1935867/847",
+                      "https://labs.hackthebox.com/achievement/machine/1935867/867",
+                      "https://labs.hackthebox.com/achievement/machine/1935867/856",
+                      "https://labs.hackthebox.com/achievement/machine/1935867/835",
+                      "https://labs.hackthebox.com/achievement/machine/1935867/814"]
+
+# __________________________HackTheBox Challanges 6 семестр__________________________
+image_htb_chal_6sem = ["/image/6sem/cyber/chal/Lucky Dice.svg",
+                       "/image/6sem/cyber/chal/Execute.svg",
+                       "/image/6sem/cyber/chal/ShinyHunter.svg",
+                       "/image/6sem/cyber/chal/Restaurant.svg",
+                       "/image/6sem/cyber/chal/Touch.svg"]
+
+name_htb_chal_6sem = ["Lucky Dice",
+                      "Execute",
+                      "ShinyHunter",
+                      "Restaurant",
+                      "Touch"]
+
+link_htb_chal_6sem = ["https://labs.hackthebox.com/achievement/challenge/1935867/1180",
+                      "https://labs.hackthebox.com/achievement/challenge/1935867/640",
+                      "https://labs.hackthebox.com/achievement/challenge/1935867/701",
+                      "https://labs.hackthebox.com/achievement/challenge/1935867/200",
+                      "https://labs.hackthebox.com/achievement/challenge/1935867/390"]
+
+# __________________________ПКтаС 6 семестр__________________________
+
+# __________________________PicoCTF 6 семестр__________________________
+image_pico_6sem = ["/image/6sem/crypto/picoctf/interencdec.png",
+                      "/image/6sem/crypto/picoctf/mod26.png",
+                      "/image/6sem/crypto/picoctf/thenumbers.png",
+                      "/image/6sem/crypto/picoctf/vigenere.png",
+                      "/image/6sem/crypto/picoctf/13.png",
+                      "/image/6sem/crypto/picoctf/rsa_oracle.png",
+                      "/image/6sem/crypto/picoctf/customenc.png",
+                      "/image/6sem/crypto/picoctf/c3.png"]
+
+name_pico_6sem = ["Interencdec",
+                 "Mod 26",
+                 "The numbers",
+                 "Vigenere",
+                 "13",
+                 "RSA_oracle",
+                 "Custom encryption",
+                 "C3"]
---
+++ site.py
@@ -0,0 +1,190 @@
+import streamlit as st
+
+tab = st.tabs(["Головна", "5 семестр", "6 семестр"])
+
+if "main" not in st.session_state:
+    st.session_state.main = "home"
+
+if "cyber" not in st.session_state:
+    st.session_state.cyber = "home"
+
+if "htb" not in st.session_state:
+    st.session_state.htb = "home"
+
+if "crypto" not in st.session_state:
+    st.session_state.crypto = "home"
+
+def render_grid(images, names, links=None, cols_count=4):
+    for row in range(0, len(images), cols_count):
+        cols = st.columns(cols_count)
+
+        for col, i in zip(cols, range(row, min(row + cols_count, len(images)))):
+            with col:
+                with st.container(border=True):
+                    st.image(images[i], use_container_width=True)
+
+                    st.markdown(
+                        f"""
+                        <div style="
+                            height:70px;
+                            display:flex;
+                            align-items:center;
+                            justify-content:center;
+                            text-align:center;
+                            font-weight:600;
+                            font-size:14px;
+                        ">
+                            {names[i]}
+                        </div>
+                        """,
+                        unsafe_allow_html=True
+                    )
+
+                    if links:
+                        st.link_button("Відкрити", links[i], use_container_width=True)
+
+# __________________________Головна__________________________
+with tab[0]:
+    st.markdown("<h1 style='text-align:center'>Вітаю на сайті портфоліо курсанта групи ННІ4-23-303Кб Мудрицького Ігоря Володимировича</h1>", unsafe_allow_html=True)
+
+# __________________________5 семестр__________________________
+with tab[1]:
+
+    disc1, disc2 = st.columns(2)
+
+    with disc1:
+        if st.button("Кібербезпека", use_container_width=True, key="cyber 5"):
+            st.session_state.main = "cyber"
+
+    with disc2:
+        st.link_button("TryHackMe", "https://tryhackme.com/p/migor2k05", use_container_width=True, key="thm 5")
+
+# __________________________Кібербезпека__________________________
+    if st.session_state.main == "cyber":
+        
+        cisco, htb = st.columns(2)
+
+        with cisco:
+            if st.button("Cisco", use_container_width=True, key="cisco 5"):
+                st.session_state.cyber = "cisco"
+
+        with htb:
+            if st.button("HackTheBox", use_container_width=True, key="htb 5"):
+                st.session_state.cyber = "htb"
+
+# __________________________Cisco__________________________
+        if st.session_state.cyber == "cisco":
+            from config import name_cisco_5sem, image_cisco_5sem, link_cisco_5sem
+            render_grid(image_cisco_5sem, name_cisco_5sem, link_cisco_5sem, 4)
+            
+            with st.container(border=True):
+                st.markdown("<h4 style='text-align:center'>Початок роботи з Cisco Packet Tracer</h4>", unsafe_allow_html=True)
+                st.image("D:/site/image/5sem/cyber/cisco/Початок роботи з Cisco Packet Tracer.png", use_container_width=True)
+
+            with st.container(border=True):
+                st.markdown("<h4 style='text-align:center'>Exploring Networking with Cisco Packet Tracer</h4>", unsafe_allow_html=True)
+                st.image("D:/site/image/5sem/cyber/cisco/Exploring Networking with Cisco Packet Tracer.png", use_container_width=True)
+
+            with st.container(border=True):
+                st.markdown("<h4 style='text-align:center'>Exploring Internet of Things with Cisco Packet Tracer</h4>", unsafe_allow_html=True)
+                st.image("D:/site/image/5sem/cyber/cisco/Exploring Internet of Things with Cisco Packet Tracer.png", use_container_width=True)
+            
+
+# __________________________HackTheBox__________________________
+        if st.session_state.cyber == "htb":
+
+            htb = st.columns(3)
+
+            with htb[0]:
+                if st.button("Machines", use_container_width=True, key="mach 5"):
+                    st.session_state.htb = "mach"
+
+            with htb[1]:
+                if st.button("Challanges", use_container_width=True, key="chal 5"):
+                    st.session_state.htb = "chal"
+
+            with htb[2]:
+                if st.button("Modules", use_container_width=True, key="mod 5"):
+                    st.session_state.htb = "mod"
+
+# __________________________HackTheBox Machines__________________________
+            if st.session_state.htb == "mach":
+                from config import image_htb_mach_5sem, name_htb_mach_5sem, link_htb_mach_5sem
+                render_grid(image_htb_mach_5sem, name_htb_mach_5sem, link_htb_mach_5sem, 4)
+
+# __________________________HackTheBox Challanges__________________________
+            if st.session_state.htb == "chal":
+                from config import image_htb_chal_5sem, name_htb_chal_5sem, link_htb_chal_5sem
+                render_grid(image_htb_chal_5sem, name_htb_chal_5sem, link_htb_chal_5sem, 4)
+
+# __________________________HackTheBox Modules__________________________
+            if st.session_state.htb == "mod":
+                from config import image_htb_mod_5sem, name_htb_mod_5sem, link_htb_mod_5sem
+                render_grid(image_htb_mod_5sem, name_htb_mod_5sem, link_htb_mod_5sem, 4)
+
+# __________________________6 семестр__________________________
+with tab[2]:
+    
+    disc = st.columns(2)
+
+    with disc[0]:
+        if st.button("Кібербезпека", use_container_width=True, key = "cyber 6"):
+            st.session_state.main = "cyber"
+
+    with disc[1]:
+        if st.button("ПКтаС", use_container_width=True, key = "crypto 6"):
+            st.session_state.main = "crypto"
+# __________________________Кібербезпека__________________________
+    if st.session_state.main == "cyber":
+        
+        col = st.columns(3)
+
+        with col[0]:
+            if st.button("Modules", use_container_width=True, key="mod 6"):
+                st.session_state.htb = "mod"
+
+        with col[1]:
+            if st.button("Machines", use_container_width=True, key="mach 6"):
+                st.session_state.htb = "mach"
+
+        with col[2]:
+            if st.button("Challanges", use_container_width=True, key="chal 6"):
+                st.session_state.htb = "chal"
+# __________________________HackTheBox Modules 6 семестр__________________________
+        if st.session_state.htb == "mod":
+            from config import image_htb_mod_6sem, name_htb_mod_6sem, link_htb_mod_6sem
+            render_grid(image_htb_mod_6sem, name_htb_mod_6sem, link_htb_mod_6sem, 4)
+
+# __________________________HackTheBox Machines 6 семестр__________________________
+        if st.session_state.htb == "mach":
+            from config import image_htb_mach_6sem, name_htb_mach_6sem, link_htb_mach_6sem
+            render_grid(image_htb_mach_6sem, name_htb_mach_6sem, link_htb_mach_6sem, 4)
+
+# __________________________HackTheBox Challanges 6 семестр__________________________
+        if st.session_state.htb == "chal":
+            from config import image_htb_chal_6sem, name_htb_chal_6sem, link_htb_chal_6sem
+            render_grid(image_htb_chal_6sem, name_htb_chal_6sem, link_htb_chal_6sem, 4)
+
+# __________________________ПКтаС 6 семестр__________________________
+    if st.session_state.main == "crypto":
+
+        cols = st.columns(3)
+
+        with cols[0]:
+            st.link_button("TryHackMe", "https://tryhackme.com/p/migor2k05", use_container_width=True, key="thm 6")
+
+        with cols[1]:
+            st.link_button("CryptoHack", "https://cryptohack.org/user/MudritskiyIgor/", use_container_width=True, key="cryptohack 6")
+
+        with cols[2]:
+            if st.button("PicoCTF", use_container_width=True, key="picoctf 6"):
+                st.session_state.crypto = "pico"
+
+# __________________________PicoCTF 6 семестр__________________________
+        if st.session_state.crypto == "pico":
+            from config import image_pico_6sem, name_pico_6sem
+
+            for i in range(0, len(image_pico_6sem)):
+                with st.container(border=True):
+                    st.markdown(f"<h4 style='text-align:center'>{name_pico_6sem[i]}</h4>", unsafe_allow_html=True)
+                    st.image(image_pico_6sem[i], use_container_width=True)
