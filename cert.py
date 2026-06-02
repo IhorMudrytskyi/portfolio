38e02e152979c1892807628cb44fd80575f49a42
IhorMudrytskyi <migor2k05@gmail.com>
2026-06-02 11:35:18 +0300

    Add files via upload

---
+++ cert.py
@@ -0,0 +1,43 @@
+name = [
+"ChatGPT для підвищення власної ефективності",
+"CS50 Вступ до кібербезпеки",
+"QA-тестувальник",
+"Базові знання з кібергігієни",
+"Безбар_єрна грамотність",
+"Відкрий для себе кар’єрні шляхи в ІТ",
+"Гендерна рівність та соціальна інклюзія в комунікаціях",
+"Держава без бар'єрів",
+"Кібергігієна для молоді",
+"Кібергігієна як захиститися від фішингу",
+"Онлайн-інструменти протидії домашньому насильству",
+"Школа OSINT",
+"Як захиститися від фейків і дезінформації"]
+
+image = []
+
+for i in range(0, len(name)):
+    image.append("https://raw.githubusercontent.com/IhorMudrytskyi/portfolio/main/image/cert/" + name[i] + ".pdf")
+
+name2 = ["A Beginner_s Guide to Linux Kernel Development",
+         "Android Bug Bounty Hunting",
+         "CS50 Вступ до штучного інтелекту з Python",
+         "Cybersecurity for Businesses - The Fundamental Edition",
+         "Foundational Course on Counter-Terrorism in the UN System",
+         "Make In-house Hacking and Pentesting Lab",
+         "SQL Injection Attacks",
+         "Вирішую конфлікти та будую мир навколо себе",
+         "Декларуй ДоброЧесно! 2020",
+         "Запобігання торгівлі людьми",
+         "Захист прав людей з інвалідністю",
+         "Комунікація щодо національних спільнот для пресслужб поліції та прокуратури",
+         "Перемовини про вступ України до ЄС",
+         "Про НАТО",
+         "Тестування до Дня Гідності та Свободи",
+         "Україна на шляху до ЄС",
+         "Я знаю гендер",
+         "Introduction to Dark Web, Anonymity, and Cryptocurrency"]
+
+image2 = []
+
+for i in range(0, len(name2)):
+    image2.append("https://raw.githubusercontent.com/IhorMudrytskyi/portfolio/main/image/cert/" + name2[i] + ".pdf")
